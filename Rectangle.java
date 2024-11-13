7268f6f2a9520ee586f03b8a401f26bb04276a68
Ruan Oliveira da Silva <69682432+Dracofer@users.noreply.github.com>
2024-11-13 20:46:31 -0300

    Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Rectangle {
+  public double Widht;
+  public double Height;
+  public double Area;
+  public double Perimeter;
+  public double Diagonal;
+  
+  public double Area() {
+    return Widht * Height;
+
+  }
+  public double Perimeter() {
+	  return 2*(Widht + Height);
+
+  }
+  public double Diagonal() {
+    return Math.sqrt((Widht*Widht) + (Height*Height));
+
+  }
+}
